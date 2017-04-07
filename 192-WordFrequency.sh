b92507f3a7a51fbf4479874696b18b3dcfd76faa
ascott97 <alex97scott@gmail.com>
2017-04-07 09:16:38 +0100

    Better way to do 192

--- 192-WordFrequency.sh
+++ 192-WordFrequency.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-for i in $(cat words.txt); do echo $i; done | sort | uniq -c  | sort -nr | awk '{ i = $1; $1 = $2; $2 = i; print}'
+for i in $(cat words.txt); do echo $i; done | sort | uniq -c  | sort -nr | awk '{ print $2 " " $1 }' 
